d19e2dd33a4b0818047962ed4ac2c07ebd150874
Chenaa <dimobix@gmail.com>
2021-05-15 18:17:00 +0300

    15.05.21

--- Linux/faq/ProductController.php
+++ Linux/faq/ProductController.php
@@ -1,44 +1,107 @@
 <?php
 
-namespace app\controllers;
 
-use app\models\Breadcrumbs;
-use app\models\Product;
+namespace app\controllers\admin;
 
-class ProductController extends AppController {
 
-    public function viewAction(){
-        $alias = $this->route['alias'];
-        $product = \R::findOne('product', "alias = ? AND status = 1", [$alias]);
-        if(!$product){
-            throw new \Exception('Страница не найдена', 404);
+use app\models\admin\Product;
+use app\models\AppModel;
+use ishop\App;
+use ishop\libs\Pagination;
+
+class ProductController extends AppController{
+    public function indexAction(){
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perpage = 10;
+        $count = \R::count('product');
+        $pagination = new Pagination($page, $perpage, $count);
+        $start = $pagination->getStart();
+        $products = \R::getAll("SELECT product.*, category.title AS cat FROM product
+JOIN category ON category.id = product.category_id ORDER BY product.title LIMIT $start, $perpage");
+        $this->setMeta('Список товаров');
+        $this->set(compact('products','pagination','count'));
+
+
+    }
+
+    public function addImageAction(){
+        if(isset($_GET['upload'])){
+            if($_POST['name'] == 'single'){
+                $wmax = App::$app->getProperty('img_width');
+                $hmax = App::$app->getProperty('img_height');
+
+            }else{
+                $wmax = App::$app->getProperty('gallery_width');
+                $hmax = App::$app->getProperty('gallery_height');
+            }
+            $name = $_POST['name'];
+            $product = new Product();
+            $product->uploadImg($name, $wmax, $hmax);
         }
+    }
 
-        // хлебные крошки
-        $breadcrumbs = Breadcrumbs::getBreadcrumbs($product->category_id, $product->title);
+    public function editAction(){
+        if(!empty($_POST)){
 
-        // связанные товары
-        $related = \R::getAll("SELECT * FROM related_product JOIN product ON product.id = related_product.related_id WHERE related_product.product_id = ?", [$product->id]);
-
-        // запись в куки запрошенного товара
-        $p_model = new Product();
-        $p_model->setRecentlyViewed($product->id);
-
-        // просмотренные товары
-        $r_viewed = $p_model->getRecentlyViewed();
-        $recentlyViewed = null;
-        if($r_viewed){
-            $recentlyViewed = \R::find('product', 'id IN (' . \R::genSlots($r_viewed) . ') LIMIT 3', $r_viewed);
         }
+        $id = $this->getRequestID();
+        $product = \R::load('product', $id);
+        App::$app->setProperty('parent_id', $product->category_id);
+        $filter = \R::getCol("SELECT attr_id FROM attribute_product WHERE product_id = ?", [$id]);
+        $related_product = \R::getAll("SELECT related_product.related_id, product.title FROM related_product JOIN product ON product.id = related_product.related_id WHERE related_product.product_id = ?", [$id]);
+        $gallery = \R::getCol("SELECT img FROM gallery WHERE product_id = ?", [$id]);
+        $this->setMeta("Редактирование товара {$product->title}");
+        $this->set(compact('product', 'filter','related_product', 'gallery'));
 
-        // галерея
-        $gallery = \R::findAll('gallery', 'product_id = ?', [$product->id]);
+    }
 
-        // модификации
-        $mods = \R::findAll('modification', 'product_id = ?', [$product->id]);
+    public function addAction(){
 
-        $this->setMeta($product->title, $product->description, $product->keywords);
-        $this->set(compact('product', 'related', 'gallery', 'recentlyViewed', 'breadcrumbs', 'mods'));
+        if(!empty($_POST)){
+            $product = new Product();
+            $data = $_POST;
+            //debug($data, 1);
+            $product->load($data);
+            $product->attributes['status'] = $product->attributes['status'] ? '1' : '0';
+            $product->attributes['hit'] = $product->attributes['hit'] ? '1' : '0';
+            $product->getImg();
+            if(!$product->validate($data)){
+                $product->getErrors();
+                $_SESSION['form_data'] = $data;
+                redirect();
+            }
+
+            if($id = $product->save('product')){
+                $product->saveGallery($id);
+                $alias = AppModel::createAlias('product','alias', $data['title'], $id);
+                $p = \R::load('product', $id);
+                $p->alias = $alias;
+                \R::store($p);
+                $product->editFilter($id, $data);
+                $product->editRelatedProduct($id, $data);
+                $_SESSION['success'] = 'Товар успешно добавлен';
+            }
+            redirect();
+        }
+        $this->setMeta('Новый товар');
+
+
+    }
+
+    public function relatedProductAction(){
+        $q = isset($_GET['q']) ? $_GET['q'] : '';
+        $data['items'] = [];
+        $products = \R::getAssoc('SELECT id, title FROM product WHERE title LIKE ? LIMIT 10', ["%{$q}%"]);
+        if($products){
+            $i = 0;
+            foreach($products as $id => $title){
+                $data['items'][$i]['id'] = $id;
+                $data['items'][$i]['text'] = $title;
+                $i++;
+            }
+            echo json_encode($data);
+            die;
+        }
     }
 
 }
